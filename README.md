30b018b762d5975b211f71ea38cecd53c85cd0a9
sindhuja1999 <gampasindhuja999@gmail.com>
2021-08-11 13:39:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-usadqa-sdqwaz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-usadqa-sdqwaz)
---
+++ utils/sample-data.ts
@@ -0,0 +1,36 @@
+import { User } from '../interfaces'
+
+/** Dummy user data. */
+export const sampleUserData: User[] = [
+  { id: 1,
+  keyword: 'Blockchain',
+  goal: 'technology',
+  matches:18,
+  search_status:'Done' },
+  { id: 2,
+    keyword: 'ReactJs',
+    goal: 'technology',
+    matches:18,
+    search_status:'Done'  },
+  {id: 3,
+    keyword: 'NextJs',
+    goal: 'technology',
+    matches:18,
+    search_status:'Done'  },
+  { id: 4,
+    keyword: 'Typescript',
+    goal: 'technology',
+    matches:18,
+    search_status:'Done' },
+    { id: 5,
+      keyword: 'JavaScript',
+      goal: 'technology',
+      matches:18,
+      search_status:'Done' },
+      { id: 6,
+        keyword: 'Django',
+        goal: 'technology',
+        matches:18,
+        search_status:'Done' },
+]
+
